306f90f4961a7cd830a12cfbd6ce1d9cbba6ec53
Ayoub Kochtben <ayoubkochtbene@gmail.com>
2026-09-10 19:45:20 +0100

    fix angular production environment

--- vente-project/frontend-angular/src/environments/environment.prod.ts
+++ vente-project/frontend-angular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-   export const environment = {
-     production: true,
-     apiUrl: 'https://olivenwood-production.up.railway.app/api'
-   };
+export const environment = {
+  production: true,
+  apiUrl: 'https://olivenwood-production.up.railway.app/api'
+};
